d5f6770f1775aef3d12ab8db55c4015fc861531c
Araz Shahinian <arshahinian@gmail.com>
2022-01-09 11:51:47 -0500

    Done

--- index.js
+++ index.js
@@ -5,7 +5,24 @@ const pc = newPlayableCharacter(100, 110)
 const npc = newNonPlayableCharacter(50, 300)
 
 // have the NPC start walking east immediately
-npc.walkEast()
+
+var arrayController = [
+    {Dircection: "N",MiliSeconds: 1400}
+    ,
+    {Dircection: "E",MiliSeconds: 1200}
+    ,
+    {Dircection: "S",MiliSeconds: 300}
+    ,
+    {Dircection: "E",MiliSeconds: 1500}
+    ,
+    {Dircection: "S",MiliSeconds: 1500}
+    ,
+    {Dircection: "W",MiliSeconds: 2700}
+    ,
+    {Dircection: "N",MiliSeconds: 400}
+];
+
+moveNPC(arrayController)
 
 // Create the inventory
 const inventory = newInventory()
@@ -20,3 +37,37 @@ move(newImage('assets/well.png')).to(500, 575)
 move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
+
+
+async function moveNPC(arrayController){
+    
+    for(let i = 0;i<arrayController.length;i++)
+    {
+        let controller = arrayController[i];
+        switch(controller.Dircection)
+        {
+            case "N":{
+                await npc.walkNorth(controller.MiliSeconds)
+                console.log(controller.Dircection+":"+controller.MiliSeconds)
+                break
+            }
+            case "E":{
+                await npc.walkEast(controller.MiliSeconds)
+                console.log(controller.Dircection+":"+controller.MiliSeconds)
+                break
+            }
+            case "W":{
+                await npc.walkWest(controller.MiliSeconds)
+                console.log(controller.Dircection+":"+controller.MiliSeconds)
+                break
+            }
+            case "S":{
+                await npc.walkSouth(controller.MiliSeconds)
+                console.log(controller.Dircection+":"+controller.MiliSeconds)
+                break
+            }
+        }     
+
+    }
+    
+}
